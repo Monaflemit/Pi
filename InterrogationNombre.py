d8c286e19570e857a66d2328a19a153eb454cb6c
Monaflemit <108365633+Monaflemit@users.noreply.github.com>
2024-06-14 00:27:30 +0200

    Update InterrogationNombre.py
    Rajout de e phi et racine de 2

--- InterrogationNombre.py
+++ InterrogationNombre.py
@@ -28,3 +28,88 @@ st.write(f"Nombre de décimales correctes avant la première erreur : {correct_d
 
 
 
+st.title("Vérification des décimales de e")
+
+e="718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059921817413596629043572900334295260595630"
+
+string3 = st.text_input("Entrez les décimales de e que vous connaissez : ")
+
+correct_decimals3 = 0
+
+texte3 = ""
+for i in range(min(len(e), len(string3))):
+    if e[i] == string3[i]:
+        texte3 += f"<span style='color:green '>{string3[i]}</span>"
+        if not("red" in texte):
+            correct_decimals3 += 1
+    else:
+        texte3 += f"<span style='color:red'>{string3[i]}</span>"
+
+# Afficher le texte avec les balises HTML
+st.markdown(texte3, unsafe_allow_html=True)
+
+st.write(f"Nombre de décimales correctes avant la première erreur : {correct_decimals3}")
+
+
+st.title("Vérification des décimales de φ")
+
+phi="6180339887498948482045868343656381177203091798057628621354486227052604628189024497072072041893911374847540880753868917521266338622235369317931800607667263"
+
+# Entrée utilisateur pour les décimales de φ
+string4 = st.text_input("Entrez les décimales de φ que vous connaissez :")
+
+correct_decimals4 = 0
+texte4 = ""
+for i in range(min(len(phi), len(string4))):
+    if phi[i] == string4[i]:
+        texte4 += f"<span style='color:green'>{string4[i]}</span>"
+        if not("red" in texte):
+            correct_decimals4 += 1
+    else:
+        texte4 += f"<span style='color:red'>{string4[i]}</span>"
+
+# Afficher le texte avec les balises HTML
+st.markdown(texte4, unsafe_allow_html=True)
+st.write(f"Nombre de décimales correctes avant la première erreur : {correct_decimals4}")
+
+
+st.title("Vérification des décimales de √2")
+
+# Décimales connues de √2
+sqrt2 = "4142135623730950488016887242096980785696718753769480731766797379907324784621070388503875343276415727"
+         
+
+# Entrée utilisateur pour les décimales de √2
+string5 = st.text_input("Entrez les décimales de √2 que vous connaissez :")
+
+correct_decimals5 = 0
+texte5 = ""
+for i in range(min(len(sqrt2), len(string5))):
+    if sqrt2[i] == string5[i]:
+        texte5 += f"<span style='color:green'>{string5[i]}</span>"
+        if not("red" in texte):
+            correct_decimals5 += 1
+    else:
+        texte5 += f"<span style='color:red'>{string5[i]}</span>"
+
+# Afficher le texte avec les balises HTML
+st.markdown(texte5, unsafe_allow_html=True)
+st.write(f"Nombre de décimales correctes avant la première erreur : {correct_decimals5}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
